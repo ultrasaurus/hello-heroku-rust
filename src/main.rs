b8a4891a5014189b8396c84563e0091b88d19865
Sarah Allen <sarah@ultrasaurus.com>
2020-01-04 14:10:25 -0800

    delete unused

--- src/main.rs
+++ src/main.rs
@@ -22,33 +22,3 @@ async fn main() -> io::Result<()> {
         println!("sent response\n\n\n");
     }
 }
-
-// #[tokio::main]
-// async fn main() {
-//     let addr = "127.0.0.1:80";
-//     let mut listener = TcpListener::bind(addr).await.unwrap();
-
-//     // Here we convert the `TcpListener` to a stream of incoming connections
-//     // with the `incoming` method. We then define how to process each element in
-//     // the stream with the `for_each` combinator method
-//     let server = async move {
-//         let mut incoming = listener.incoming();
-//         while let Some(socket_res) = incoming.next().await {
-//             match socket_res {
-//                 Ok(socket) => {
-//                     println!("Accepted connection from {:?}", socket.peer_addr());
-//                     socket.write_all("<h1>Hello!</h1>")
-//                 }
-//                 Err(err) => {
-//                     // Handle error by printing to STDOUT.
-//                     println!("accept error = {:?}", err);
-//                 }
-//             }
-//         }
-//     };
-
-//     println!("Server running on localhost");
-
-//     // Start the server and block this async fn until `server` spins down.
-//     server.await;
-// }
