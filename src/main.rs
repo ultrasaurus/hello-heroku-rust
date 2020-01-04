db5ce3a3597c23d576e8ae55de03315cea31d352
Sarah Allen <sarah@ultrasaurus.com>
2020-01-04 14:29:48 -0800

    configure port

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 // example code from:
 // https://docs.rs/tokio/0.2.6/tokio/net/struct.TcpListener.html
 // https://docs.rs/tokio/0.2.6/tokio/net/struct.TcpStream.html
+use std::env;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut listener = TcpListener::bind("127.0.0.1:3000").await?;
+    // Get the port number to listen on (required for heroku deployment).
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
 
+    let addr = format!("127.0.0.1:{}", port);
+    let mut listener = TcpListener::bind(addr).await?;
+
+    println!("listening on port {}...", port);
     loop {
-        println!("waiting for a request...");
         let (socket, _) = listener.accept().await?;
-        println!("accepted socket!");
         let mut request = String::new();
         let mut buffed_socket = tokio::io::BufReader::new(socket);
         buffed_socket.read_line(&mut request).await?;
