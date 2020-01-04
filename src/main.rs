3ff5e965fa15d90210d3bd3eb1e10227512829b8
Sarah Allen <sarah@ultrasaurus.com>
2020-01-04 15:38:51 -0800

    handle errors, no panic except when failing to bind to port

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,24 @@ async fn main() -> io::Result<()> {
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
 
     let addr = format!("127.0.0.1:{}", port);
-    let mut listener = TcpListener::bind(addr).await?;
+    let mut listener = TcpListener::bind(addr).await.unwrap();
 
     println!("listening on port {}...", port);
     loop {
-        let (socket, _) = listener.accept().await?;
+        let (socket, _) = listener.accept().await.unwrap();
         let mut request = String::new();
         let mut buffed_socket = tokio::io::BufReader::new(socket);
-        buffed_socket.read_line(&mut request).await?;
-        println!("request: {}", request);
-        buffed_socket
-            .write_all(b"HTTP/1.1 200\n\n<h1>Hello!</h1>")
-            .await?;
-        println!("sent response\n\n\n");
+        let read_result = buffed_socket.read_line(&mut request).await;
+        if let Err(e) = read_result {
+            println!("failed to read from socket, err: {}", e);
+        } else {
+            println!("request: {}", request);
+            let write_result = buffed_socket
+                .write_all(b"HTTP/1.1 200\n\n<h1>Hello!</h1>")
+                .await;
+            if let Err(e) = write_result {
+                println!("failed to write, err: {}", e);
+            }
+        }
     }
 }
